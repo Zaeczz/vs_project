3350bd2f51b05bc2e6a2828b1775be48825c6cbb
Danikdbnet <95984132+Danikdbnet@users.noreply.github.com>
2021-12-20 15:13:43 +0100

    transferring code to methods

--- Program.cs
+++ Program.cs
@@ -1,99 +1,139 @@
-using System;
+//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Pupa-i-Lupa-i-Pupa">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
 namespace Test
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Fulfills all requirements for a triangle project
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Determines whether a triangle exists
+        /// </summary>
+        public static bool Existing(double ab, double ac, double bc)
         {
-            //Testing Marks
-            //Program žádá uživatele o zadání souřadnic bodu
-            Console.Write($"Enter x for point of triangle A: ");
-            double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Enter y for point of triangle A: ");
-            double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Enter x for point of triangle B: ");
-            double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Enter y for point of triangle B: ");
-            double y2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Enter x for point of triangle C: ");
-            double x3 = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Enter y for point of triangle C: ");
-            double y3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("_________________________________\n");
-
-            //Určuje délky 3 stran pomocí Pythagorovy věty 
-            double AB = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-            double AC = Math.Sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-            double BC = Math.Sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
-
-            //Zavádí dvě proměnné, které budou použity v dalších výpočtech 
-            double Square = 0;
-            double Perimetr;
-
-            if (AB + AC > BC && AC + BC > AB && BC + AB > AC)
+            bool isExist = false;
+            if (ab + ac >= bc && ac + bc >= ab && bc + ab >= ac)
             {
-                //Vypisuje souradnice trech pointu
-                Console.WriteLine("Triangle exist and has 3 points:\n");                            
-                Console.WriteLine($"Point A ({x1},{y1})");
-                Console.WriteLine($"Point B ({x2},{y2})");
-                Console.WriteLine($"Point C ({x3},{y3})");
-                Console.WriteLine("_________________________________\n");
+                IsExist = true;
+            }
 
-                //Výpočet obvodu trojúhelníku
-                Perimetr = (AB + AC + BC);
-                Perimetr = Math.Round(Perimetr, 2);
-                Console.WriteLine($"Perimeter is\t{Perimetr}");
+            return isExist;
+        }
 
-                //Výpočet plochy trojúhelníku
-                double s = (AB + AC + BC) / 2;
-                Square = Math.Sqrt(s * (s - AB) * (s - AC) * (s - BC));
-                Square = Math.Round(Square, 2);
-                Console.WriteLine($"Square is \t{Square}");
-                Console.WriteLine("_________________________________\n");
+        /// <summary>
+        /// Determines whether the triangle is rectangular
+        /// </summary>
+        public static bool Rectangular(double ab, double ac, double bc)
+        {
+            bool isRectangular = false;
 
-                 //Redukce desetinných míst na dvě
-                AB = Math.Round(AB, 2);
-                AC = Math.Round(AC, 2);
-                BC = Math.Round(BC, 2);       
+            // Vypočítá délku nejdelší strany
+            double hypotinuse = Math.Max(ab, Math.Max(ac, bc));
 
-                //Vypíše délku tří stran
-                Console.WriteLine($"AB side has \t{AB} points");
-                Console.WriteLine($"AC side has \t{AC} points");
-                Console.WriteLine($"BC side has \t{BC} points");
-                Console.WriteLine("_________________________________\n");
+            // Výpočet plochy trojúhelníku
+            double square;
+            double s = (ab + ac + bc) / 2;
+            square = Math.Sqrt(s * (s - ab) * (s - ac) * (s - bc));
+            square = Math.Round(square, 2);
 
-                //Vypočítá délku nejdelší strany
-                double Hypotinuse = Math.Max(AB, Math.Max(AC, BC));
-                
-                double katet1 = 0;
-                double katet2 = 0;
+            // Určuje pravoúhlý trojúhelník nebo ne
+            double katet1 = 0;
+            double katet2 = 0;
+            if (ab != hypotinuse)
+            {
+                katet1 = ab;
+            }
 
-                //Kontroluje zda je trouhelnik pravouhly.
-                if (AB != Hypotinuse) { katet1 = AB; }
-                if (AC != Hypotinuse)
+            if (ac != hypotinuse)
+            {
+                if (katet1 == 0)
                 {
-                    if (katet1 == 0) katet1 = AC;
-                    else katet2 = AC;
+                    katet1 = ac;
                 }
-                if (BC != Hypotinuse) { katet2 = BC; }
-
-                //Vypíše, že trojúhelník není pravoúhlý
-                if ((katet1 * katet2) / 2 == Square) 
+                else
                 {
-                    Console.WriteLine("This triangle is rectangular");
+                    katet2 = ac;
                 }
-                else //Vypíše, že trojúhelník není pravoúhlý
-                {    
-                    Console.WriteLine("This triangle is not rectangular");
-                }
-                
+            }
 
+            if (bc != hypotinuse)
+            {
+                katet2 = bc;
+            }
+
+            if ((katet1 * katet2) / 2 == square)
+            {
+                isRectangular = true;
             }
             else
             {
-                Console.WriteLine("Triangle doesn't exist");
-            }
-            Console.ReadLine();
+                isRectangular = false;                
+
+
+            return isRectangular;
         }
-    }
-}
+
+        /// <summary>
+        /// Uses the above methods. Requires the user to specify the coordinates of the points. Outputs the response.
+        /// </summary>
+        public static void Main()
+        {
+            // Testing Marks
+            double number;
+
+            // Program žádá uživatele o zadání souřadnic bodu a testuje jestli jich napsal spravne
+            double x1 = 0, x2 = 0, x3 = 0, y1 = 0, y2 = 0, y3 = 0;
+
+            Console.Write($"Enter x for point of triangle A: ");
+            string temp1 = Console.ReadLine();
+            bool isParsable = double.TryParse(temp1, out number);
+            if (isParsable)
+            {
+                x1 = double.Parse(temp1);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct number");
+                Main();
+            }
+
+            Console.Write($"Enter y for point of triangle A: ");
+            string temp2 = Console.ReadLine();
+            isParsable = double.TryParse(temp2, out number);
+            if (isParsable)
+            {
+                y1 = double.Parse(temp2);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct number");
+                Main();
+            }
+
+            Console.Write($"Enter x for point of triangle B: ");
+            string temp3 = Console.ReadLine();
+            isParsable = double.TryParse(temp3, out number);
+            if (isParsable)
+            {
+                x2 = double.Parse(temp3);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct number");
+                Main();
+            }
+
+            Console.Write($"Enter y for point of triangle B: ");
+            string temp4 = Console.ReadLine();
+            isParsable = double.TryParse(temp4, out number);
+            if (isParsable)
+            {
+                y2 = double.Parse(temp4);
+            }
